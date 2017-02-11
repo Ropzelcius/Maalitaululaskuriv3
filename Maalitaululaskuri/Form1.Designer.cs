f61c68df29812853f0849dc679a2289f3eb1d5e3
Ropzelcius <ropzelcius@gmail.com>
2017-02-12 01:19:09 +0200

    Meemer

---
+++ Maalitaululaskuri/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Maalitaululaskuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(714, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(12, 513);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ota kuva";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(658, 542);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Turquoise;
+            this.button2.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button2.Location = new System.Drawing.Point(126, 513);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Vertaa";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Turquoise;
+            this.button3.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button3.Location = new System.Drawing.Point(240, 513);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 46);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(452, 513);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "TOINEN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(452, 526);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "KOLMAS:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(452, 497);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "TOINEN:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(952, 498);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "150";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(952, 542);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "40";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(848, 497);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Pistetarkkuus";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label5.Location = new System.Drawing.Point(848, 544);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 18);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Kuvatarkkuus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(1375, 594);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Maalitaululaskuri/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge;
+using AForge.Video;
+using System.IO;
+
+namespace Maalitaululaskuri
+{
+    public partial class Form1 : Form
+    {
+        int pistetarkkuus = 0;
+        int kuvatarkkuus = 0;
+        int keskipiste = 0;
+        int toinen = 0;
+        int kolmas = 0;
+        int tulosmax=0;
+        int tulostoinen = 0;
+        int tuloskolmas = 0;
+        private FilterInfoCollection CaptureDevice; 
+        private VideoCaptureDevice FinalFrame;
+        Bitmap x;
+        Bitmap y;
+        Color color = Color.GreenYellow;
+        Color color2 = Color.Red;
+        Color color3 = Color.Purple;
+        Color color4 = Color.Blue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+            }
+
+
+
+            comboBox1.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image!=null)
+            {
+                pictureBox2.Image.Dispose();
+            }
+            
+            pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
+            if (x==null)
+            {               
+               x= (Bitmap)pictureBox1.Image.Clone();
+
+            }
+            else
+            {
+                y= (Bitmap)pictureBox1.Image.Clone();
+            }
+             
+
+        }
+
+        void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs) 
+                                                                             
+        {                   
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            
+
+        }
+        public Bitmap getDifferencBitmap(Bitmap bmp1, Bitmap bmp2, Color diffColor)
+        {
+            int leveys = bmp1.Width;
+            int korkeus = bmp1.Height;
+            leveys = leveys / 2;
+            korkeus = korkeus / 2;
+            
+
+            double gap = korkeus * 0.20;
+            
+            bool aaa;
+            bool rrr;
+            bool ggg;
+            bool bbb;
+            Size s1 = bmp1.Size;
+            Size s2 = bmp2.Size;
+            if (s1 != s2) return null;
+
+            Bitmap bmp3 = new Bitmap(s1.Width, s1.Height);
+
+            for (int y = 0; y < s1.Height; y++)
+                for (int x = 0; x < s1.Width; x++)
+                {
+                    aaa  = false;
+                    rrr  = false;
+                    ggg = false;
+                    bbb = false;
+                    Color c1 = bmp1.GetPixel(x, y);
+                    Color c2 = bmp2.GetPixel(x, y);
+                    int c1a = c1.A;
+                    int c1R= c1.R;
+                    int c1G = c1.G;
+                    int c1B = c1.B;
+
+                    int c2A = c2.A;
+                    int c2R = c2.R;
+                    int c2G = c2.G;
+                    int c2B = c2.B;
+                    int aa = c1a - c2A;
+                    int rr = c1R - c2R;
+                    int gg = c1G - c2G;
+                    int bb = c1B - c2B;
+                    if(aa >5 ||aa < -5)
+                    {
+                        aaa = true;
+                    }
+                   
+                    if (rr > kuvatarkkuus|| rr < -kuvatarkkuus)
+                    {
+                        rrr = true;
+                    }
+                    if (gg > kuvatarkkuus || gg < -kuvatarkkuus)
+                    {
+                        ggg = true;
+                    }
+                    if (bb > kuvatarkkuus || bb < -kuvatarkkuus)
+                    {
+                        bbb = true;
+                    }
+                    if (aaa==true)
+                    {
+                        bmp3.SetPixel(x, y, diffColor);
+                        pisteet(x, y,leveys,korkeus);
+                    }
+                    if (rrr==true)
+                    {
+                        bmp3.SetPixel(x, y, diffColor);
+                        pisteet(x, y, leveys, korkeus);
+                    }
+                    else if(ggg==true)
+                    {
+                        bmp3.SetPixel(x, y, diffColor);
+                        pisteet(x, y, leveys, korkeus); ;
+                    }
+                    else if (bbb==true)
+                    {
+                        bmp3.SetPixel(x, y, diffColor);
+                        pisteet(x, y, leveys, korkeus);
+                    }
+                    else
+                    {
+                        bmp3.SetPixel(x, y, c1);
+                        
+                    }
+
+                    if (x == leveys - gap&& y>korkeus-gap && y<korkeus+gap || x == leveys + gap&& y>korkeus-gap && y<korkeus+gap)
+                    {
+                        bmp3.SetPixel(x, y, color2);
+                    }
+                    if (y == korkeus - gap && x > leveys - gap && x < leveys + gap || y == korkeus + gap && x> leveys - gap && x < leveys + gap)
+                    {
+                        bmp3.SetPixel(x, y, color2);
+
+                    }
+                    if (x == leveys + gap * 2 && y > korkeus - gap*2 && y < korkeus + gap*2 || x == leveys - gap * 2 && y > korkeus - gap*2 && y < korkeus + gap*2)
+                    {
+                        bmp3.SetPixel(x, y, color3);
+                    }
+
+                    if (y == korkeus + gap * 2 && x > leveys - gap*2 && x < leveys + gap *2|| y == korkeus - gap * 2 && x > leveys - gap*2 && x < leveys + gap*2)
+                    {
+                        bmp3.SetPixel(x, y, color3);
+                    }
+                    if (x == leveys + gap * 4 && y > korkeus - gap * 4 && y < korkeus + gap * 4 || x == leveys - gap * 4 && y > korkeus - gap * 4 && y < korkeus + gap * 4)
+                    {
+                        bmp3.SetPixel(x, y, color4);
+                    }
+                    if (y == korkeus + gap * 4 && x > leveys - gap * 4 && x < leveys + gap * 4 || y == korkeus - gap * 4 && x > leveys - gap * 4 && x < leveys + gap * 4)
+                    {
+                        bmp3.SetPixel(x, y, color4);
+                    }
+                }
+            if (keskipiste > pistetarkkuus)
+            {
+                tulosmax = tulosmax + 1;
+                label1.Text = "EKA: "+tulosmax.ToString();
+            }
+            if (toinen > pistetarkkuus)
+            {
+               tulostoinen = tulostoinen + 1;
+                label3.Text = "TOINEN: "+tulostoinen.ToString();
+            }
+            if (kolmas > pistetarkkuus)
+            {
+                tuloskolmas = tuloskolmas + 1;
+                label4.Text = "KOLMAS: " + tulostoinen.ToString();
+            }
+            return bmp3;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kuvatarkkuus = int.Parse(textBox2.Text);
+            pistetarkkuus = int.Parse(textBox1.Text);
+            Bitmap diff= getDifferencBitmap(x, y, color);
+            pictureBox2.Image = diff;
+        }
+        private void pisteet(int x, int y, int leveys, int korkeus)
+        {
+            double gap = korkeus*0.10;
+            if (x>leveys-gap&&x<leveys+gap)
+            {
+                if (y>korkeus-gap && y<korkeus+gap)
+                {
+                    keskipiste++;
+                }
+            }
+
+            if (x > leveys + gap && x < leveys + gap*2 || x > leveys - gap * 2  && x < leveys - gap)
+            {
+               
+                  if (y > korkeus + gap && y < korkeus + gap * 2 || y > korkeus - gap * 2  && y < korkeus - gap)
+                  {
+                    toinen++;
+                  }
+               
+            }
+            if (x > leveys + gap*2 && x < leveys + gap * 4 || x > leveys - gap *4 && x < leveys - gap*2)
+            {
+
+                if (y > korkeus + gap*2 && y < korkeus + gap * 4 || y > korkeus - gap * 4 && y < korkeus - gap*2)
+                {
+                    kolmas++;
+                }
+
+            }
+
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            x = null;
+            y = null;
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            FinalFrame.Stop();
+        }
+
+
+    }
+}
